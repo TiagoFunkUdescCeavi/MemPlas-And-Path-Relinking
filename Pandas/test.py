84c0fb6de3dbf5102cfaa21668d099a7533d95cd
Tiago Funk <tiagoff.tf@gmail.com>
2019-10-01 12:42:29 -0300

    Examples no pandas

--- Pandas/test.py
+++ Pandas/test.py
@@ -5,3 +5,9 @@ print( state_of_art.head() )
 
 results = pd.read_csv('../Logs/results.csv')
 print( results.head() )
+
+shallow_now = pd.merge(results, state_of_art, how ='inner', on ='instance')
+print( shallow_now )
+
+group = results.groupby(['instance']).get_group('AfricaSul11e')
+print( group['result'].mean() )
--- Scripts/experiment.sh
+++ Scripts/experiment.sh
@@ -12,7 +12,6 @@ do
 	if [ -f "$file" ]
 	then
 		echo "$file"
-		echo -n "$file," >> $log
 		./../Project/Debug/Project 1 1 1 "$PWD/$file" --strategy m --sizePopulation 150 --elite  0.3 --limitIterations 100 --sizePlasmideo 0.2 --cross 0.4 >> $log && echo "Ok"
 	fi
 done
