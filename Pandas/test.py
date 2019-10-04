1794d79310a2c6d91234206cfad427138df55370
Tiago Funk <tiagoff.tf@gmail.com>
2019-10-04 18:57:40 -0300

    Preparing for the final experiment

--- Pandas/test.py
+++ Pandas/test.py
@@ -1,13 +1,33 @@
 import pandas as pd
 
-state_of_art = pd.read_csv('../expected.csv')
-print( state_of_art.head() )
+# state_of_art = pd.read_csv('../expected.csv')
 
-results = pd.read_csv('../Logs/results.csv')
-print( results.head() )
+# results = pd.read_csv('../Logs/results.csv')
 
-shallow_now = pd.merge(results, state_of_art, how ='inner', on ='instance')
-print( shallow_now )
+# together = pd.merge(results, state_of_art, how ='inner', on ='instance')
 
-group = results.groupby(['instance']).get_group('AfricaSul11e')
-print( group['result'].mean() )
+# groups = together.groupby(['instance'])
+
+# average = groups.mean()
+# print( average )
+
+
+data = pd.read_csv("../Logs/test.csv")
+print( data )
+print( '>>>>>>>>>' )
+
+groups = data.groupby(['key'])
+
+average = groups['value'].mean().reset_index()
+print( average )
+print( '>>>>>>>>>' )
+
+minimun = groups['value'].min().reset_index()
+print( minimun )
+print( '>>>>>>>>>' )
+
+final = pd.merge(average, minimun, how ='inner', on ='key')
+# final = pd.merge(final, data['key','best'], how ='inner', on ='key')
+# final.columns = ['key','mean','min']
+print( final )
+print( '>>>>>>>>>' )
