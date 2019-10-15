259d93c9aaa60e991f3c72c31a6f3b7dacb4fec9
Tiago Funk <tiagoff.tf@gmail.com>
2019-10-15 15:17:19 -0300

    path relink experiments

---
+++ Scripts/experiment_path_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $HOME
+cd "Repositorios/Github/MemPlas-And-Path-Relinking/Instances/"
+
+log=$HOME"/Repositorios/Github/MemPlas-And-Path-Relinking/Logs/results_path_2.csv"
+
+echo "instance,strategy,result,time" > $log
+for n in $(seq 1 8)
+do
+    for file in *
+    do
+	    if [ -f "$file" ]
+	    then
+		    echo "$n-$file"
+		    ./../Project/Debug/Project 1 1 1 "$PWD/$file" --strategy pr --sizePopulation 215 --elite 0.55 --limitIterations 2000 --selectionStrategy a --intermediaryStrategy stef >> $log && echo "Ok"
+	    fi
+    done
+done
