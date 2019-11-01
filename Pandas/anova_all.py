6538ff08a1f47165991cc37884bdcaeec8efb0fb
Tiago Funk <tiagoff.tf@gmail.com>
2019-11-01 15:49:12 -0300

    Tukey Test

--- Pandas/anova_all.py
+++ Pandas/anova_all.py
@@ -2,6 +2,8 @@ import sys
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from statsmodels.stats.multicomp import MultiComparison
 
 data1 = pd.read_csv( sys.argv[1] )
 data2 = pd.read_csv( sys.argv[2] )
@@ -12,18 +14,21 @@ together = pd.concat([data1, data2, data3, data4 ])
 
 groups = together.groupby(["instance",'strategy'])
 
+other_groups = together.groupby(["instance"])
+
 instances = together["instance"].unique()
 algorithms = together["strategy"].unique()
 
 for inst in instances:
-    print("aa")
     fvalue, pvalue = stats.f_oneway( 
         groups.get_group( (inst,"m") )["result"], 
         groups.get_group( (inst,"pr") )["result"], 
         groups.get_group((inst,"mpr"))["result"],
-        groups.get_group((inst,"ols"))["result"] )
-    print( "instances: ", inst, " p-value: ", pvalue )
+        groups.get_group((inst,"ols"))["result"] 
+    )
+    print( inst+";"+str(round(pvalue,4)) )
 
     if pvalue < 0.05:
-        print("Tukey!!")
-    
+        mc = MultiComparison( other_groups.get_group(inst)["result"], other_groups.get_group(inst)["strategy"] )
+        mc_results = mc.tukeyhsd()
+        print(mc_results.summary())
