f40be43eb3b874161affba022b02579ee252500d
Tiago Funk <tiagoff.tf@gmail.com>
2019-11-08 15:25:11 -0300

    Second experiment is ready

---
+++ Scripts/21-experiment_only_memplas_path_relinking_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $HOME
+cd "Repositorios/Github/MemPlas-And-Path-Relinking/Instances/"
+
+log=$HOME"/Repositorios/Github/MemPlas-And-Path-Relinking/Logs/results_only_memplas_path_relinking_2.csv"
+
+echo "instance,strategy,result,time" > $log
+for n in $(seq 1 8)
+do
+    for file in *
+    do
+	    if [ -f "$file" ]
+	    then
+		    echo "$n-$file"
+		    ./../Project/Debug/Project 1 1 1 "$PWD/$file" --strategy ompr --sizePopulation 200 --elite 0.5 --sizePlasmideo 0.5 --cross 0.5 --limitIterations 2450 --selectionStrategy a --intermediaryStrategy rf >> $log && echo "Ok"
+	    fi
+    done
+done
+
